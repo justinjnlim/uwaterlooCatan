c776369e6eef21e1b4057a51f0fecbdff1b5ff65
justinjnlim <justinjnlim@gmail.com>
2017-04-02 02:50:32 -0400

    board attach

--- board.cc
+++ board.cc
@@ -10,6 +10,49 @@ Board::Board() {
   properties.emplace_back(make_shared<Property>(5));
   attach(tiles[0]);
   tiles[0]->attach(properties[0]);
+
+  // LINK THESE MOTHERFUCKERS
+  // PROPERTY LINKING
+  for(int i = 0; i < 19; ++i) {
+    int start = 2*i;
+    if (i > 5) ++start;
+    if (i > 7) ++start;
+    if (i > 10) ++start;
+    if (i > 12) ++start;
+    if (i > 15) start += 3;
+    // TODO WRITE THIS ADDPROPERTY FUNCTION
+    tiles[i].addProperty(properties[start]);
+    tiles[i].addProperty(properties[start+1]);
+    if (i == 0) {
+      tiles[i].addProperty(properties[start+3]);
+      tiles[i].addProperty(properties[start+4]);
+      tiles[i].addProperty(properties[start+8]);
+      tiles[i].addProperty(properties[start+9]);
+    } else if (i < 3) {
+      tiles[i].addProperty(properties[start+5]);
+      tiles[i].addProperty(properties[start+6]);
+      tiles[i].addProperty(properties[start+11]);
+      tiles[i].addProperty(properties[start+12]);
+    } else if (i < 16) {
+      tiles[i].addProperty(properties[start+6]);
+      tiles[i].emplace_back(properties[start+7]);
+      tiles[i].emplace_back(properties[start+12]);
+      tiles[i].emplace_back(properties[start+13]);
+    } else if (i < 18) {
+      tiles[i].emplace_back(properties[start+6]);
+      tiles[i].emplace_back(properties[start+7]);
+      tiles[i].emplace_back(properties[start+11]);
+      tiles[i].emplace_back(properties[start+12]);
+    } else {
+      tiles[i].emplace_back(properties[start+5]);
+      tiles[i].emplace_back(properties[start+6]);
+      tiles[i].emplace_back(properties[start+8]);
+      tiles[i].emplace_back(properties[start+9]);
+    }
+  }
+
+
+
 }
 
 shared_ptr<Property> Board::buildProperty(int id, shared_ptr<Player> player) {
